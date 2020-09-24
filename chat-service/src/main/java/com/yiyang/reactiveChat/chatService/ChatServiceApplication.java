53d380c37d63d5bd40b62dd798df28c2755a6d4d
yliang04 <64179584+yliang04@users.noreply.github.com>
2020-09-24 14:48:50 -0400

    added configurations

--- chat-service/src/main/java/com/yiyang/reactiveChat/chatService/ChatServiceApplication.java
+++ chat-service/src/main/java/com/yiyang/reactiveChat/chatService/ChatServiceApplication.java
@@ -2,8 +2,11 @@ package com.yiyang.reactiveChat.chatService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringCloudApplication
+@EnableEurekaClient
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
