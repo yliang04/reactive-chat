fe9cdc63a3e60cca5b445f27ab50ebca2f54dd6d
yliang04 <64179584+yliang04@users.noreply.github.com>
2020-09-24 14:25:06 -0400

    Initial commit

---
+++ chat-service/src/test/java/com/yiyang/reactiveChat/chatService/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiyang.reactiveChat.chatService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ config-server/src/test/java/com/yiyang/reactiveChat/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiyang.reactiveChat.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
