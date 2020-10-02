d7d2667d1c5c16fffe2c06c90767b749fb538e78
yliang04 <64179584+yliang04@users.noreply.github.com>
2020-10-02 14:51:25 -0400

    added security config
    disabled csrf for now. Added logoutSuccessHandler to redirect.

--- chat-service/src/main/java/com/yiyang/reactiveChat/chatService/SecurityConfig.java
+++ chat-service/src/main/java/com/yiyang/reactiveChat/chatService/SecurityConfig.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+import java.net.URI;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
         return http
+                .csrf().disable()
                 .authorizeExchange()
                 .pathMatchers(ALLOW_LIST).permitAll()
                 .anyExchange()
@@ -18,7 +24,24 @@ public class SecurityConfig {
                 .and()
                 .httpBasic()
                 .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(logoutSuccessHandler("/"))
+                .and()
                 .build();
+    }
+
+
+    /**
+     * Default logout would redirect to /login?logout. Add a custom handler to redirect to root.
+     * @param uri
+     * @return
+     */
+    private ServerLogoutSuccessHandler logoutSuccessHandler(String uri) {
+        RedirectServerLogoutSuccessHandler logoutSuccessHandler = new RedirectServerLogoutSuccessHandler();
+        logoutSuccessHandler.setLogoutSuccessUrl(URI.create(uri));
+
+        return logoutSuccessHandler;
     }
 
     private static final String[] ALLOW_LIST = {
@@ -26,7 +49,6 @@ public class SecurityConfig {
             "/",
             "/chat/**",
             "/topic/**",
-            "/login",
             "/user",
             "/**.css",
             "/**.js",
